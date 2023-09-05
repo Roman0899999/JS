2d95b661feecb2c895185dbcd2a27175a725f423
Roman0899999 <rkesisan8@gmail.com>
2023-09-05 18:23:01 +0300

    ojjji

---
+++ less.js
@@ -0,0 +1,60 @@
+// let a =1;
+// const b =2;
+// let c = String(a)
+
+
+// 2
+// let age  = prompt("Ваш возраст")
+// let result = age < 18 ? "Школьник" : "Студент"
+// alert(result)
+
+// 3
+// let num = Math.floor(Match.random() * (1000 - 10 + 1 )) + 10;
+
+//   for (let num = 0; num < 10; num++) { 
+//     alert(num);
+//   }
+// let arg = prompt("Введите число?");
+// switch (arg) {
+//   case '0':
+//   case '1':
+//     alert( 'Один или ноль' );
+//     break;
+
+//   case '2':
+//     alert( 'Два' );
+//     break;
+
+//   case "3":
+//     alert( 'три' );
+//     break;
+//   case "4":
+//     alert( 'четыре' );
+//     break;
+//   case "5":
+//      alert( 'пять' );
+//      break;
+//  case "6":
+//     alert( 'шесть' );
+//      break;      
+//      case "7":
+//         alert( 'семь' );
+//         break;
+//          case "8":
+//         alert( 'восемь' );
+//         break;         
+//   default:
+//     alert( 'нафиг так много' );
+// }
+
+
+
+let pal = 10;
+while (pal > 0) {
+    let num = Math.round(Math.random(100)*100)
+    console.log(num)
+    pal-- 
+    
+  }
+
+
